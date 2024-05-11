49a2cd8fdbbe87aafd866525d1355cb167167da0
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-05-11 17:53:45 +0300

    first commit

---
+++ src/main/java/com/example/discoveryservice/DiscoveryserviceapplicationApplication.java
@@ -0,0 +1,16 @@
+package com.example.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+
+public class DiscoveryserviceapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryserviceapplicationApplication.class, args);
+	}
+
+}
