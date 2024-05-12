bf08ccb320fb33109e71734f0097e97737a1f748
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-05-12 18:07:32 +0300

    fisrt commit cicd

--- src/main/java/com/example/discoveryservice/DiscoveryserviceapplicationApplication.java
+++ src/main/java/com/example/discoveryservice/DiscoveryserviceapplicationApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-
 public class DiscoveryserviceapplicationApplication {
 
 	public static void main(String[] args) {
